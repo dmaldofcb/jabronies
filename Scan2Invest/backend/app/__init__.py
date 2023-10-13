25a4fd22d625496031cc5ad635f9045b0f5b1a6b
dmaldofcb <dmaldofcb@gmail.com>
2023-10-13 18:38:23 -0400

    cors enabled

--- Scan2Invest/backend/app/__init__.py
+++ Scan2Invest/backend/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    
+    CORS(app)
     # Importing and registering Blueprints
     from .controller.scan_2_invest import scan_2_invest_bp
     app.register_blueprint(scan_2_invest_bp)
