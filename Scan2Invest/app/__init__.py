1f47fd71cc4f4742db0e63edbd0359c104e3143f
dmaldofcb <dmaldofcb@gmail.com>
2023-10-15 16:39:04 -0400

    moved cors import

--- Scan2Invest/app/__init__.py
+++ Scan2Invest/app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_cors import CORS
 from app.controller import scan_2_invest
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
     # Importing and registering Blueprints
     app.register_blueprint(scan_2_invest.scan_2_invest_bp)
     
